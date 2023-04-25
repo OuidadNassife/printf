61a5be1adfd9a694033c9a722a568ee64aa130df
NassifeOuidad <nassife.ouidad@gmail.com>
2023-04-25 09:33:50 -0700

    main.c

--- main.c
+++ main.c
@@ -7,11 +7,8 @@
  */
 int main(void)
 {
-<<<<<<< HEAD
 _printf("%b\n", 98);
 return (0);
-=======
-    _printf("%b\n", 98);
-    return (0);
->>>>>>> 1411dfbb0961fda75b018175bebf5041810e87fe
+_printf("%b\n", 98);
+return (0);
 }
